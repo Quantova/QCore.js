2e0103e961d481a52d7a0f1a29d1d2740e9561a9
quantova-inc <Quantovaorg@gmail.com>
2026-07-19 05:35:29 +0000

    Add the pkg qcore js

---
+++ pkg/qcore_js.js
@@ -0,0 +1,9 @@
+/* @ts-self-types="./qcore_js.d.ts" */
+import * as wasm from "./qcore_js_bg.wasm";
+import { __wbg_set_wasm } from "./qcore_js_bg.js";
+
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
+export {
+    account_body, address, block_by_height_body, buildSignedOrderCall, contractAddress, eventsBody, mapSlotKey, mnemonicFromSeed, nonceSlotKey, orderSigner, parseEvents, scalarSlotKey, seedFromMnemonic, signRegister, sign_call, sign_transfer, storageBody, storageValue, submit_body, transaction_body, valid_address, vmDeployAddress
+} from "./qcore_js_bg.js";
