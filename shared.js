1c4f87a3a5fcb249288feb37a7e8e975f879aa4f
quantova-inc <Quantovaorg@gmail.com>
2026-09-01 20:34:17 +0800

    point the testnet network preset at the live chain id

--- shared.js
+++ shared.js
@@ -146,7 +146,7 @@ class Network {
   static testnet() {
     return new Network({
       name: 'testnet',
-      chainId: 'Q-test-net-1',
+      chainId: 'Q-test-net-2',
       rpcUrl: 'https://rpc-testnet.quantova.org',
       explorerUrl: 'https://qvmscan.io',
       isMainnet: false,
